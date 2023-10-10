dedaa04779aa3c45cce82b70626187307ce108a6
Boston <BostonCEssary@gmail.com>
2023-10-10 16:45:30 -0700

    add event listener to enemy board so UI changes depeneding if it was hit or miss

--- src/dom.js
+++ src/dom.js
@@ -1,6 +1,6 @@
-function drawPlayerBoard(player, id) {
+function drawPlayerBoard(player) {
   const playerBoard = document.createElement('div');
-  playerBoard.id = id;
+  playerBoard.id = 'player-board';
   playerBoard.classList.add('board');
 
   for (let i = 0; i < player.gameboard.board.length; i++) {
@@ -10,6 +10,8 @@ function drawPlayerBoard(player, id) {
       const spot = document.createElement('div');
       spot.classList.add('spot');
       spot.innerText = player.gameboard.board[i][j];
+      spot.dataset.yCord = i;
+      spot.dataset.xCord = j;
       newRow.append(spot);
     }
     playerBoard.append(newRow);
@@ -17,4 +19,49 @@ function drawPlayerBoard(player, id) {
   return playerBoard;
 }
 
-export default { drawPlayerBoard };
+function drawEnemyBoard(player) {
+  const playerBoard = document.createElement('div');
+  playerBoard.id = 'enemy-board';
+  playerBoard.classList.add('board');
+
+  for (let i = 0; i < player.gameboard.board.length; i++) {
+    const newRow = document.createElement('div');
+    newRow.classList.add('row');
+    for (let j = 0; j < player.gameboard.board[i].length; j++) {
+      const spot = document.createElement('div');
+      spot.classList.add('spot');
+      spot.classList.add('enemy-spot');
+      spot.innerText = player.gameboard.board[i][j];
+      spot.dataset.yCord = i;
+      spot.dataset.xCord = j;
+      spot.addEventListener('click', () => {
+        const enemyBoardContainer = document.getElementById('enemy-board-container');
+        const yCord = parseInt(spot.dataset.yCord);
+        const xCord = parseInt(spot.dataset.xCord);
+        player.gameboard.recieveAttack(xCord, yCord);
+        enemyBoardContainer.innerHTML = '';
+        enemyBoardContainer.append(drawEnemyBoard(player));
+      });
+      newRow.append(spot);
+    }
+    playerBoard.append(newRow);
+  }
+  return playerBoard;
+}
+
+function handleEnemyBoardClicks(attackingPlayer, attackedPlayer) {
+  const enemyBoardContainer = document.getElementById('enemy-board-container');
+
+  const enemySpots = document.querySelectorAll('.enemy-spot');
+  enemySpots.forEach((spot) => {
+    spot.addEventListener('click', () => {
+      const yCord = parseInt(spot.dataset.yCord);
+      const xCord = parseInt(spot.dataset.xCord);
+      attackingPlayer.attackEnemy(attackedPlayer, xCord, yCord);
+      enemyBoardContainer.innerHTML = '';
+      enemyBoardContainer.append(drawEnemyBoard(attackedPlayer));
+    });
+  });
+}
+
+export default { drawPlayerBoard, drawEnemyBoard, handleEnemyBoardClicks };
--- src/index.js
+++ src/index.js
@@ -16,7 +16,21 @@ player2.gameboard.placeShip(player2.gameboard.patrolBoat, false, 3, 7);
 player2.gameboard.placeShip(player2.gameboard.destroyer, true, 7, 4);
 player2.gameboard.placeShip(player2.gameboard.submarine, false, 7, 0);
 
-player1.gameboard.recieveAttack(0, 0);
-player2.gameboard.recieveAttack(0, 0);
-document.body.prepend(dom.drawPlayerBoard(player1, 'player-board'));
-document.body.prepend(dom.drawPlayerBoard(player2, 'enemy-board'));
+player1.attackEnemy(player2, 0, 0);
+
+const enemyBoardContainer = document.createElement('div');
+enemyBoardContainer.id = 'enemy-board-container';
+enemyBoardContainer.append(dom.drawEnemyBoard(player2, player1));
+document.body.prepend(dom.drawPlayerBoard(player1));
+document.body.prepend(enemyBoardContainer);
+
+const enemySpots = document.querySelectorAll('.enemy-spot');
+enemySpots.forEach((spot) => {
+  spot.addEventListener('click', () => {
+    const yCord = parseInt(spot.dataset.yCord);
+    const xCord = parseInt(spot.dataset.xCord);
+    player1.attackEnemy(player2, xCord, yCord);
+    enemyBoardContainer.innerHTML = '';
+    enemyBoardContainer.append(dom.drawEnemyBoard(player2));
+  });
+});
